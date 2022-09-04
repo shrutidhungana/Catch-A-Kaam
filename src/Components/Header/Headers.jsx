cef8dbea0fe2d454ff681f55dc15c169d161bc96
shrutidhungana <shrutidhungana@prixa.net>
2022-09-04 14:00:15 +0545

    Only one logo sufficient

--- src/Components/Header/Headers.jsx
+++ src/Components/Header/Headers.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo2 from "../../Assets/logo2.png";
+import logo1 from "../../Assets/logo1.png";
 import { AiOutlineMenu } from "react-icons/ai";
 import { Header, Image, Heading, Button } from './HeaderStyle';
 import './Header.css'
@@ -8,7 +8,7 @@ import './Header.css'
 const Headers = () => {
   return (
       <Header>
-          <Image src={logo2} alt="logo" />
+          <Image src={logo1} alt="logo" />
           <Heading>Catch A Kaam</Heading>
         <Button>Login</Button>
         <AiOutlineMenu className="menu" />
