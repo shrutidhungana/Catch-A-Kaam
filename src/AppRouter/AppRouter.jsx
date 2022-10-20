7ac8f8095dbf20dd511eab70da5048ebdc9fc5b3
shrutidhungana <shrutidhungana@prixa.net>
2022-10-20 13:08:52 +0545

    Hosting

--- src/AppRouter/AppRouter.jsx
+++ src/AppRouter/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "../Components/Header/Headers";
 import NavBar from "../Components/NavBar/NavBar";
 import Homepage from "../Components/Homepage/Homepages";
